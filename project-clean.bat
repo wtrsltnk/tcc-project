667c6ac2e3abb8e2b3cbce6f7c9b5c045f0524cf
Wouter Saaltink <wouter.saaltink@gmail.com>
2016-08-20 01:12:49 +0200

    changed tcc-project name

--- project-clean.bat
+++ project-clean.bat
@@ -4,39 +4,40 @@ setlocal EnableDelayedExpansion
 for %%i in ("%~dp0..") do (
     set "PROJECT_NAME=%%~ni"
     set "PROJECT_ROOT=%%~dpni"
+    set "TCC_PROJECT_ROOT=%PROJECT_ROOT%\tcc-project"
     call %%~di
     cd %%~dpni
 )
 
-set OBJ_DIR=%PROJECT_ROOT%\tccproject\obj
-set BIN_DIR=%PROJECT_ROOT%\tccproject\bin
+set OBJ_DIR=%TCC_PROJECT_ROOT%\obj
+set BIN_DIR=%TCC_PROJECT_ROOT%\bin
 
 echo.
-echo @echo Cleaning project %PROJECT_NAME%>%PROJECT_ROOT%\tccproject\temp_clean.bat
+echo @echo Cleaning project %PROJECT_NAME%>%TCC_PROJECT_ROOT%\temp_clean.bat
 
 if exist %OBJ_DIR% (
     @rem Delete all objects
-    echo @echo.>>%PROJECT_ROOT%\tccproject\temp_clean.bat
-    echo @echo Cleaning compiled objects:>>%PROJECT_ROOT%\tccproject\temp_clean.bat
+    echo @echo.>>%TCC_PROJECT_ROOT%\temp_clean.bat
+    echo @echo Cleaning compiled objects:>>%TCC_PROJECT_ROOT%\temp_clean.bat
     for /f "tokens=*" %%a in (%PROJECT_ROOT%\project-files.txt) do (
-        echo @del /f %OBJ_DIR%\%%~na.o>>%PROJECT_ROOT%\tccproject\temp_clean.bat
+        echo @del /f %OBJ_DIR%\%%~na.o>>%TCC_PROJECT_ROOT%\temp_clean.bat
     )
 )
 
 if exist %BIN_DIR% (
     @rem Delete the exe
-    echo @echo.>>%PROJECT_ROOT%\tccproject\temp_clean.bat
-    echo @echo Cleaning executable:>>%PROJECT_ROOT%\tccproject\temp_clean.bat
-    echo @del /f %BIN_DIR%\%PROJECT_NAME%.exe>>%PROJECT_ROOT%\tccproject\temp_clean.bat
+    echo @echo.>>%TCC_PROJECT_ROOT%\temp_clean.bat
+    echo @echo Cleaning executable:>>%TCC_PROJECT_ROOT%\temp_clean.bat
+    echo @del /f %BIN_DIR%\%PROJECT_NAME%.exe>>%TCC_PROJECT_ROOT%\temp_clean.bat
 )
 
-echo @echo.>>%PROJECT_ROOT%\tccproject\temp_clean.bat
-echo @echo All done.>>%PROJECT_ROOT%\tccproject\temp_clean.bat
+echo @echo.>>%TCC_PROJECT_ROOT%\temp_clean.bat
+echo @echo All done.>>%TCC_PROJECT_ROOT%\temp_clean.bat
 
 @echo on
-@call %PROJECT_ROOT%\tccproject\temp_clean.bat
+@call %TCC_PROJECT_ROOT%\temp_clean.bat
 @echo off
-@del %PROJECT_ROOT%\tccproject\temp_clean.bat
+@del %TCC_PROJECT_ROOT%\temp_clean.bat
 
 goto :exit 
 
--- project-run.bat
+++ project-run.bat
@@ -4,17 +4,18 @@ setlocal EnableDelayedExpansion
 for %%i in ("%~dp0..") do (
     set "PROJECT_NAME=%%~ni"
     set "PROJECT_ROOT=%%~dpni"
+    set "TCC_PROJECT_ROOT=%PROJECT_ROOT%\tcc-project"
     call %%~di
     cd %%~dpni
 )
 
-set BIN_DIR=%PROJECT_ROOT%\tccproject\bin
+set BIN_DIR=%TCC_PROJECT_ROOT%\bin
 if not exist %BIN_DIR%\%PROJECT_NAME%.exe goto :no_binary_found
 
 call %BIN_DIR%\%PROJECT_NAME%.exe
 goto :exit 
 
 :no_binary_found
-echo There is no project binary, run tccproject\project-build.bat to build your project
+echo There is no project binary, run tcc-roject\project-build.bat to build your project
 
 :exit
